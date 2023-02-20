cad03a39dd9d4cf4a079dcef257a7a9f0dcf00a8
Marlosh <arstailev@gmail.com>
2023-02-20 22:37:58 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+app.listen(port, () => {
+  console.log(`Сервер запущен на порту ${port}`);
+});
